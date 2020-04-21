7d3699e99acba917536c0bf1e6a7e478062e11ee
crushyna <crushyna@gmail.com>
2020-04-21 18:23:52 +0200

    testing fast azure deployment

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ from generate_image_function.generate_image_function import VoiceImageGenerator
 app_main = Flask(__name__)
 api = Api(app_main)
 
-api.add_resource(VoiceVerification, '/verify_voice/<string:user_login>/<string:filename>/<int:text_id>')
+# api.add_resource(VoiceVerification, '/verify_voice/<string:user_login>/<string:filename>/<int:text_id>')
 api.add_resource(VoiceVerificationTest, '/verify_voice_test')
-api.add_resource(VoiceArrayUploader, '/array_upload/<int:user_id>/<string:filename>/<int:text_id>')
-api.add_resource(VoiceImageGenerator, '/image_generator/<int:user_id>/<int:text_id>')
+# api.add_resource(VoiceArrayUploader, '/array_upload/<int:user_id>/<string:filename>/<int:text_id>')
+# api.add_resource(VoiceImageGenerator, '/image_generator/<int:user_id>/<int:text_id>')
 
 app_main.run(port=5500, debug=True)
+
+if __name__ == '__main__':
+    app_main.run(port=5000, debug=True)

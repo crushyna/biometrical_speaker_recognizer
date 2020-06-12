c4baf900a20b990aa2a70f6928ff5aea65247969
piotrec <piotrec@op.pl>
2020-06-13 01:23:37 +0200

    - handling path of config file instead of hardcoded address

--- app/Config.py
+++ app/Config.py
@@ -1,10 +1,11 @@
 import configparser
+import os
 
 
 class BasicAuth:
     def __init__(self):
         config = configparser.ConfigParser()
-        config.read("config.ini")
+        config.read(os.getcwd() + "/app/config.ini")
         self.login = config['BASIC_AUTH']['login']
         self.password = config['BASIC_AUTH']['pass']
 

66aabed59f020fc242505ba3f96ddf1e5b034c79
crushyna <crushyna@gmail.com>
2020-06-12 20:03:42 +0200

    array upload fix

--- app/functions/upload_array_function/upload_array_main.py
+++ app/functions/upload_array_function/upload_array_main.py
@@ -20,14 +20,14 @@ class VoiceArrayUploader(Resource):
         new_voice_array_exist = os.path.isfile(os.path.join(WorkingFolders.upload_folder, local_filename))
         if not new_voice_array_exist:
             return {'message': f'Filename: {local_filename} does not exists on back-end server!',
-                    'status': 'error'}
+                    'status': 'error'}, 404
 
         # retrieve filepath/filename for storing data on server
         filename_to_upload = VoiceArrayModel.get_remote_destination(new_voice_array.merchant_id, new_voice_array.user_id, new_voice_array.text_id)
 
         if filename_to_upload is False:
             return {'message': 'Cannot establish connection to the database server!',
-                    'status': 'error'}
+                    'status': 'error'}, 400
 
         # transform input wavefile
         input_sound = SoundPreprocessor(os.path.join(WorkingFolders.upload_folder, local_filename))

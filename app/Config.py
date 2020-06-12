605930250160264cdbcf958e9c75298da36c867e
piotrec <piotrec@op.pl>
2020-06-12 21:20:58 +0200

    - BasicAuth class added

---
+++ app/Config.py
@@ -0,0 +1,11 @@
+import configparser
+
+
+class BasicAuth:
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read("config.ini")
+        self.login = config['BASIC_AUTH']['login']
+        self.password = config['BASIC_AUTH']['pass']
+
+

3dd312499edc914acc121bae0659af79a50e33ed
Radek M <46869083+crushyna@users.noreply.github.com>
2020-06-14 15:46:31 +0200

    added GetTextsInfoByUserId

--- app/helpers/front_registration.py
+++ app/helpers/front_registration.py
@@ -32,3 +32,18 @@ class GetTextsRandom(Resource):
                     'status': 'error'}, 403
 
         return response.json()
+
+
+class GetTextsInfoByUserId(Resource):
+
+    def get(self, merchant_id: int, user_id: int):
+        from json import JSONDecodeError
+        url = f"https://dbapi.pl/texts/info/byUserId/{merchant_id}/{user_id}"
+        basic_auth = Config.BasicAuth()
+        try:
+            response = requests.request("GET", url, auth=(basic_auth.login, basic_auth.password))
+        except JSONDecodeError:
+            return {'message': 'Database error!',
+                    'status': 'error'}, 403
+
+        return response.json()
